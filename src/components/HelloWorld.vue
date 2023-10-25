4b5a8ae2c60901e31dd852f1087dca844bc8bb37
77yes <yegorov.serg@znu.edu.ua>
2023-10-25 14:52:00 +0300

    Перший коміт: додано Vue.js додаток.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Єгоров Сергій Юрійович</h1>
+  <div class="card">
+    <p>Інженерія програмного забезпечення</p>
+    <p>2 курс</p>
+    <p>група 8.1212-з</p>
+  
+    <button type="button" @click="count++">count is {{ count }}</button>
+   
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
